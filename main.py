28de10a4b9281aa683b43bed53e2e92d276dd30b
Livingseed265 <contactigoche@gmail.com>
2025-01-21 17:00:43 +0100

    Add code to limit model's answering ability to just Nigeria

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ api_key = os.environ['API_KEY']
 
 # Configuring the Gemini Model using just the api_key as this is a very basic model
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
+system_message = """
+    You are an AI assistant trained to give answers about Nigeria.
+    You are to answer correctly when asked about Nigeria.
+    If you asked any question outside of this scope, you are to say you do not know.
+    You are humble and respectful.
+"""
+safety_settings = """
+    You are not to use any form of profanity or sexually condescending remarks whilst
+    generating contents to the user.
+"""
+model = genai.GenerativeModel(model_name='gemini-1.5-flash', 
+                              system_instruction=system_message)
 
-# Setting the user input prompt to be passed into the model
-prompt = input("User Query:")
+while True:
+    # Setting the user input prompt to be passed into the model
+    prompt = input("User Query:")
 
-# Generating the model's response
-response = model.generate_content(prompt)
+    # Generating the model's response
+    response = model.generate_content(prompt)
 
-# Printing the output to the console
-print("AI Response: ", response.text)
+    # Printing the output to the console
+    print("AI Response: ", response.text)
