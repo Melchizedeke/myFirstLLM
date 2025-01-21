6cabe12594958e45c8e12ea2b61ed13f4669bd11
Livingseed265 <contactigoche@gmail.com>
2025-01-21 12:30:21 +0100

    Add the codes to the main.py file

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-ECHO is on.
+# The required libraries import
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+# Loading  the API_KEY which is saved as an environment variable in the ".env" file
+load_dotenv()
+api_key = os.environ['API_KEY']
+
+# Configuring the Gemini Model using just the api_key as this is a very basic model
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+# Setting the user input prompt to be passed into the model
+prompt = input("User Query:")
+
+# Generating the model's response
+response = model.generate_content(prompt)
+
+# Printing the output to the console
+print("AI Response: ", response.text)
